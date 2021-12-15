3c5e426878623dbb23b18f24027ce4e8f4d2dc97
bradleyjosephh <bradleyjosephh@gmail.com>
2021-12-14 19:08:27 -0800

    all tests passing

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ function questionPrompts() {
             choices: [
                 "Manager",
                 "Engineer",
-                "Intern",
+                "Intern"
             ]
         },
     ])
